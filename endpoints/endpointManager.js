08b370385a7024d48651e241f0457ffaf4fc10ec
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-09-25 19:28:33 -0400

    User logger for callback

--- endpoints/endpointManager.js
+++ endpoints/endpointManager.js
@@ -9,8 +9,8 @@ const port = 3001;
 
 function initialize() {
     app.listen(port, (err) => {
-        if (err) { console.log(err); }
-        console.log(`Listening to port ${port}`);
+        if (err) { logger.logError(err); }
+        logger.logInfo(`Listening to port ${port}`);
     });
 
     app.use(bodyParser.json());
